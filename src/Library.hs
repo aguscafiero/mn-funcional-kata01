1af0178642ba59c2bb756a5046ab077fb115b0e2
Ezequiel Vargas <ezequielvgs@gmail.com>
2019-03-21 12:19:30 -0300

    Cambio a Stack

---
+++ src/Library.hs
@@ -0,0 +1,8 @@
+module Library
+    ( calcuLoco
+    ) where
+
+import Lib
+
+calcuLoco :: Integer -> Integer -> Integer
+calcuLoco n1 n2 = 0
---
+++ test/Spec.hs
@@ -0,0 +1,21 @@
+import Lib
+import Library
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ do
+   describe "Test calcuLoco" $ do
+      it "el primer número es mayor que el segundo" $ do
+          (calcuLoco 5 2) `shouldBe` 10
+
+      it "el primer número es mayor que el segundo - valor límite" $ do
+          (calcuLoco 5 0) `shouldBe` 0
+  
+      it "el primer número es par y es menor que el segundo" $ do
+         (calcuLoco 4 7) `shouldBe` 12
+
+      it "el primer número es par y es menor que el segundo - valor límite" $ do
+         (calcuLoco 0 7) `shouldBe` 0
+
+      it "el primer número no es par y es menor que el segundo" $ do
+         (calcuLoco 9 10) `shouldBe` (-1)
